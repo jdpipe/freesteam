53a75da25adbe2f0495bb85d2a1a545eadc379b5
jdpipe <jdpipe@2a3edb79-3362-4b52-9e0b-1d530fbd1d84>
2005-02-11 08:03:59 +0000

    Trying to make OK with GCC 3.4

--- trunk/freesteam/designbycontract.h
+++ trunk/freesteam/designbycontract.h
@@ -1,8 +1,6 @@
 #ifndef DESIGNBYCONTRACT_H
 #define DESIGNBYCONTRACT_H
 
-#ifndef NDEBUG
-
 /// Design-by-contract base class
 /**
 	Base class for all objects in the system. All classes inheriting from this class need to define a method IsValid. This method should perform a consistency check on the state of the object. Note that this method needs to be defined only when a debug build is made.
@@ -29,28 +27,31 @@ class DesignByContract {
 // program termination. The user is notified of the error condition with the right file name
 // and line number. The actual failing operation is also printed using the stringizing operator #
 
-#define ASSERT__(type,expr) if (!(expr)) DesignByContract::__throw_error_(__FILE__, __LINE__, #expr,type)
+#ifndef NDEBUG
 
-#define ASSERT(expr) ASSERT__("ASSERTION",expr)
-#define IS_VALID(obj) ASSERT__("CONSTISTENCY CHECK",(obj) != NULL && (obj)->isValid())
-#define IS_VALID_REF(obj) ASSERT__("CONSTISTENCY CHECK",(obj).isValid())
-#define REQUIRE(expr) ASSERT__("PRE-CONDITION",expr)
-#define ENSURE(expr) ASSERT__("POST-CONDITION",expr)
+	#define ASSERT__(type,expr) if (!(expr)) DesignByContract::__throw_error_(__FILE__, __LINE__, #expr,type)
 
-#define MESSAGE(msg) DesignByContract::__report_Message_(__FILE__,__LINE__,msg)
+	#define ASSERT(expr) ASSERT__("ASSERTION",expr)
+	#define IS_VALID(obj) ASSERT__("CONSTISTENCY CHECK",(obj) != NULL && (obj)->isValid())
+	#define IS_VALID_REF(obj) ASSERT__("CONSTISTENCY CHECK",(obj).isValid())
+	#define REQUIRE(expr) ASSERT__("PRE-CONDITION",expr)
+	#define ENSURE(expr) ASSERT__("POST-CONDITION",expr)
+
+	#define MESSAGE(msg) DesignByContract::__report_Message_(__FILE__,__LINE__,msg)
 
 #else
 
-// When built in release mode, the _DEBUG flag would not be defined, thus there will be no overhead
-// in the final release from these checks.
+	// When built in release mode, the _DEBUG flag would not be defined, thus there will be no overhead
+	// in the final release from these checks.
 
-#define ASSERT(expr) ((void) 0)
-#define IS_VALID(expr) ((void) 0)
-#define IS_VALID_REF(expr) ((void) 0)
-#define REQUIRE(expr) ((void) 0)
-#define ENSURE(expr) ((void) 0)
+	#define ASSERT(expr) ((void) 0)
+	#define IS_VALID(expr) ((void) 0)
+	#define IS_VALID_REF(expr) ((void) 0)
+	#define REQUIRE(expr) ((void) 0)
+	#define ENSURE(expr) ((void) 0)
 
-#define MESGAGE(msg) ((void) 0)
+	#define MESGAGE(msg) ((void) 0)
+
 #endif
 
 #endif
