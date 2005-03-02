9acec4630367f69fee06beb82e7d0dfddee7a82e
jdpipe <jdpipe@2a3edb79-3362-4b52-9e0b-1d530fbd1d84>
2005-03-02 00:05:39 +0000

    Moved inline SteamProperty::get routines to header file so that then can be compiled inline properly

--- trunk/freesteam/steamproperty.h
+++ trunk/freesteam/steamproperty.h
@@ -16,4 +16,56 @@ class SteamProperty{
 		static const char *name();
 };
 
+// 'getProperty' hooks
+
+inline SpecificEnergy
+SteamProperty<SpecificEnergy,SOLVE_IENERGY>::get(const SteamCalculator &S){
+	return S.specienergy();
+}
+
+inline SpecificEnergy
+SteamProperty<SpecificEnergy,SOLVE_ENTHALPY>::get(const SteamCalculator &S){
+	return S.specenthalpy();
+}
+
+inline Density
+SteamProperty<Density,0>::get(const SteamCalculator &S){
+	return S.dens();
+}
+
+inline SpecificVolume
+SteamProperty<SpecificVolume,0>::get(const SteamCalculator &S){
+	return S.specvol();
+}
+
+inline Temperature
+SteamProperty<Temperature,0>::get(const SteamCalculator &S){
+	return S.temp();
+}
+
+inline Pressure
+SteamProperty<Pressure,0>::get(const SteamCalculator &S){
+	return S.pres();
+}
+
+inline SpecificEntropy
+SteamProperty<SpecificEntropy,SOLVE_ENTROPY>::get(const SteamCalculator &S){
+	return S.specentropy();
+}
+
+inline SpecHeatCap
+SteamProperty<SpecHeatCap,SOLVE_CP>::get(const SteamCalculator &S){
+	return S.speccp();
+}
+
+inline SpecHeatCap
+SteamProperty<SpecHeatCap,SOLVE_CV>::get(const SteamCalculator &S){
+	return S.speccv();
+}
+
+inline Num
+SteamProperty<Num,0>::get(const SteamCalculator &S){
+	return S.quality();
+}
+
 #endif
