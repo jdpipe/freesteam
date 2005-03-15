1c478d934864ccaef609d2abb71cab1670e6e87a
jdpipe <jdpipe@2a3edb79-3362-4b52-9e0b-1d530fbd1d84>
2005-03-15 06:51:52 +0000

    Read variable names from results file

--- trunk/freesteam/emso/test/showmatrix.php
+++ trunk/freesteam/emso/test/showmatrix.php
@@ -25,79 +25,27 @@
 	x :   0.000e+000  0.000e+000  0.000e+000  0.000e+000  0.000e+000  1.705e-013  0.000e+000  0.000e+000  1.478e-012  0.000e+000  -2.000e+003  0.000e+000
 	
 */
-$f = file("php://stdin");
 
 $variables=array();
 
-// Condenser.test.mso:
-$variables[]="S.T";
-$variables[]="S.p";
-$variables[]="S.h";
-$variables[]="S.s";
-$variables[]="S.mdot";
-$variables[]="CO.Out.T";
-$variables[]="CO.Out.p";
-$variables[]="CO.Out.h";
-$variables[]="CO.Out.s";
-$variables[]="CO.Out.mdot";
-$variables[]="CO.q";
-$variables[]="CO.DT";
-$variables[]="CO.Dp";
-$variables[]="CO.UA";
+$res =& readResults("cycletest2.rlt");
 
-/*
-// turbine.test.mso
-$variables[]="S1.T";
-$variables[]="S1.p";
-$variables[]="S1.h";
-$variables[]="S1.s";
-$variables[]="S1.mdot";
-$variables[]="S2.T";
-$variables[]="S2.p";
-$variables[]="S2.h";
-$variables[]="S2.s";
-$variables[]="S2.mdot";
-$variables[]="TU.eta";
-$variables[]="TU.h_es";
+$connections=array(
+	array("PU.Out","BO.In")
+	,array("BO.Out","TU.In")
+	,array("TU.Out","CO.In")
+	,array("CO.Out","PU.In")
+);
 
-// cycle.test.mso
-$variables[]="S1.T";
-$variables[]="S1.p";
-$variables[]="S1.h";
-$variables[]="S1.s";
-$variables[]="S1.mdot";
-$variables[]="BO.Out.T";
-$variables[]="BO.Out.p";
-$variables[]="BO.Out.h";
-$variables[]="BO.Out.s";
-$variables[]="BO.Out.mdot";
-$variables[]="BO.q";
-$variables[]="BO.Dp";
-$variables[]="TU.Out.T";
-$variables[]="TU.Out.p";
-$variables[]="TU.Out.h";
-$variables[]="TU.Out.s";
-$variables[]="TU.Out.mdot";
-$variables[]="TU.eta";
-$variables[]="TU.h_es";
-$variables[]="CO.Out.T";
-$variables[]="CO.Out.p";
-$variables[]="CO.Out.h";
-$variables[]="CO.Out.s";
-$variables[]="CO.Out.mdot";
-$variables[]="CO.q";
-$variables[]="CO.DT";
-$variables[]="CO.Dp";
-$variables[]="CO.UA";
-$variables[]="PU.Out.T";
-$variables[]="PU.Out.p";
-$variables[]="PU.Out.h";
-$variables[]="PU.Out.s";
-$variables[]="PU.Out.mdot";
-$variables[]="PU.P";
-$variables[]="PU.eta";
-$variables[]="PU.v";
-*/
+$res->applyConnections($connections);
+
+$res->addIndepNodes();
+
+$variables =& $res->getIndepVarList();
+
+$f = file("php://stdin");
+
+
 
 $equations=array();
 $A=array();
@@ -178,15 +126,28 @@ while(list($n,$line)=each($f)){
 
 // Print the equations
 
-print ("\nEquations:\n");
+print ("\nEquations:\n\n");
 
 for($e=0;$e<count($equations);$e++){
-	print($equations[$e].": ");
+	//print($equations[$e].": ");
 	reset($A[$e]);
 	$first=true;
 	while(list($v,$a)=each($A[$e])){
-		if(!$first)print(" + ");
-		print(($a+0.0)." * ".$variables[$v]);
+		if($a+0.0 < 0){
+			if($first){
+				print("-");
+			}else{
+				print(" - ");
+			}
+		}else{
+			if(!$first)
+				print(" + ");
+		}
+		
+		if(abs($a+0.0)!=1.0){
+			print(abs($a+0.0)." * ");
+		}
+		print($variables[$v]);
 		$first=false;
 	}
 	print(" = ".($b[$e]+0.0)."\n");
@@ -224,12 +185,297 @@ if(count($variables) < 50 && count($equations) < 50){
 	print("\nSparsity matrix not printed: too many equations or variables (increase the limit if you want)\n");
 }
 
-print("\nReported solution:\n");
-for($v=0;$v<count($variables);$v++){
-	print($variables[$v]." = ".($x[$v]+0.0)."\n");
+if(count($x)==count($variables)){
+	print("\nReported solution:\n");
+	for($v=0;$v<count($variables);$v++){
+		print($variables[$v]." = ".($x[$v]+0.0)."\n");
+	}
+	print("\n");
+}else{
+	print("\n\nReported solution was invalid\n");
 }
-print("\n");
 
 //print_r($A);
+
+// Stuff for reading EMSO 'rlt' files...
+
+class Node{
+	var $name;
+	var $parent;
+	var $connections; // ie aliased nodes, which containing identical information
+	
+	function Node($name){
+		$this->name=$name;
+		$this->connections=array();
+		$this->parent=NULL;
+		$this->path=$this->name;
+	}
+	
+	function setParent(&$parent){
+		$this->parent=$parent;
+		
+		if($parent->path){
+			$this->path=$parent->path.".".$this->name;
+		}else{
+			$this->path=$this->name;
+		}
+	}
+	
+	function &newValueNode($name){
+		$n = new ValueNode($name);
+		$n->setParent($this);
+		$this->nodes[$name] =& $n;
+		return $n;
+	}
+
+	function &newModelNode($name){
+		$n = new ModelNode($name);
+		$n->setParent($this);
+		$this->nodes[$name] =& $n;
+		return $n;
+	}
+
+	function addVar($name,$val){
+		$n =& $this->newValueNode($name);
+		$n->value=$val;
+	}
+	
+	function getPath(){
+		return $this->path;
+	}
+	
+	function &getFlowSheet(){
+		if(is_a($this,"FlowSheet")){
+			return $this;
+		}else{
+			return $this->parent->getFlowSheet();
+		}
+	}
+		
+	function read(&$lines){
+		$numvars=trim(array_shift($lines));
+		
+		//print("Vars: $numvars\n");
+		for($i=0;$i<$numvars;$i++){
+			$varname=trim(array_shift($lines));
+			
+			//print("Var name: $varname\n");
+			
+			$data=explode(" ",array_shift($lines));
+			$varvalue=$data[3];
+			
+			$newvar =& $this->newValueNode($varname);
+			$newvar->value=$varvalue;
+			
+			//print("New var: ".$newvar->getPath()."\n");
+		}
+		
+		$numnodes=array_shift($lines);
+		for($i=0;$i<$numnodes;$i++){
+			$nodename=trim(array_shift($lines));
+			
+			//print("Node name: $nodename\n");
+			
+			$node =& $this->newModelNode($nodename);
+			$node->read($lines);
+		}
+	}
+	
+	function toString($prefix=NULL){
+		
+		foreach($this->nodes as $no){
+			$no->toString($prefix.$no->name.".");
+		}
+	}
+	
+	function &findByPathSplit($path){
+		if(count($path)==0){
+			return $this;
+		}
+		
+		$first=array_shift($path);
+		if(array_key_exists($first,$this->nodes)){
+			return $this->nodes[$first]->findByPathSplit($path);
+		}else{
+			return NULL;
+		}
+	}
+	
+	function applyConnection(&$to){
+		if(!is_array($this->connections)){
+			die("Not an array");
+		}
+		if(!is_a($to,"Node")){
+			die("Not a node");
+		}
+		
+		//print("Connecting from ".$this->getPath()."...\n");
+		//print("Connecting to ".$to->getPath()."...\n");
+		
+		for($i=0;$i<count($this->connections);$i++){
+			print("Checking ".$this->connections[$i]->getPath()."\n");
+			if($this->connections[$i]==$to){
+				die("Already connected");
+			}
+		}
+		
+		$this->connections[] =& $to;
+	}
+	
+	function isAliasOf(&$to){
+		
+		//print("Checking ".$this->getPath()." alias of ".$to->getPath()."...\n");
+		
+		if(count($this->connections)){
+			
+			//print("Checking ".count($this->connections)." connections\n");
+						
+			for($i=0;$i<count($this->connections);$i++){
+				if($this->connections[$i]->path==$to->path){
+					//print("Found ".$this->getPath()." alias of ".$to->getPath()."...\n");
+					return true;
+				}
+			}
+		}
+		
+		if($this->name!=$to->name){
+			return false;
+		}
+		
+		if(!$this->parent || $to->parent){
+			return false;
+		}
+
+		//print("Checking parents... ".$this->getPath()." alias of ".$to->getPath()."...\n");
+		
+		return $this->parent->isAliasOf($to->parent);
+	}
+	
+	function addIndepNodes(&$flowsheet){
+		
+		for($j=0;$j<count($flowsheet->indepnodes);$j++){
+			if($this->isAliasOf($flowsheet->indepnodes[$j]))return;			
+		}
+		
+		//print("Adding ".$this->getPath()."\n");
+		$flowsheet->indepnodes[] =& $this;
+		
+		if(is_a($this,"ModelNode")){
+			$keys=array_keys($this->nodes);
+			for($i=0;$i<count($keys);$i++){
+				//print($this->nodes[$keys[$i]]->getPath()."...\n");
+				$this->nodes[$keys[$i]]->addIndepNodes($flowsheet);
+			}
+		}
+	}	
+}
+
+class ValueNode extends Node{
+	var $value;
+	
+	function ValueNode($name){
+		$this->Node($name);
+		$this->value=NULL;
+	}
+	
+	function toString($prefix=NULL){
+		return("$prefix.$n = ".$v."\n");
+	}
+
+}
+
+class ModelNode extends Node{
+	var $nodes;
+
+	function toString($prefix=NULL){	
+		$str="";
+		$keys=array_keys($this->nodes);
+		for($i=0;$i<count($keys);$i++){
+			$str.=$this->nodes[$keys[$i]]->toString();
+		}
+	}	
+}
+
+class FlowSheet extends ModelNode{
+	var $indepnodes;
+	
+	function getPath(){
+		return NULL;
+	}
+	
+	function FlowSheet($name){
+		$this->ModelNode($name);
+		$this->indepvars=array();
+		$this->path="";
+	}
+	
+	function toString($prefix=NULL){
+		foreach($this->vars as $n=>$v){
+			print($n." = ".$v."\n");
+		}
+		foreach($this->nodes as $no){
+			$no->toString();
+		}
+	}
+	
+	function &findByPath($path){
+		$path1=explode(".",$path);
+		return $this->findByPathSplit($path1);
+	}
+
+	function applyConnections($connections){
+		
+		for($i=0;$i<count($connections);$i++){
+			if(count($connections[$i])!=2){
+				die("Invalid connections");
+			}
+			$frompath=$connections[$i][0];
+			$topath=$connections[$i][1];
+			
+			//print("Connecting $frompath to $topath...\n");
+			
+			$from =& $this->findByPath($frompath);
+			$to =& $this->findByPath($topath);
+			
+			if($from==NULL){
+				die("Invalid 'from'");
+			}
+			if($to==NULL){
+				die("Invalid 'to'");
+			}
+			
+			$from->applyConnection($to);
+			$to->applyConnection($from);
+		}
+	}
+	
+	function addIndepNodes(){
+		parent::addIndepNodes($this);
+	}
+	
+	function &getIndepVarList(){
+		$vars=array();
+		for($i=0;$i<count($this->indepnodes);$i++){
+			if(is_a($this->indepnodes[$i],"ValueNode")){
+				//print("Variable ".$this->indepnodes[$i]->getPath()."\n");
+				$vars[]=$this->indepnodes[$i]->getPath();
+			}
+		}
+		return $vars;
+	}
+}
+
+function readResults($filename){
+	
+	$lines = file($filename);
+	
+	$flowsheetname=trim(array_shift($lines));
+	$rubbish=array_shift($lines);
+	
+	$node =& new FlowSheet($flowsheetname);
+	$node->read($lines);
+	
+	return $node;
+}
 
 ?>
