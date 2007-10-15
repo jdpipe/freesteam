a607a1c402fc825739d265f078b75b1e6ce4269d
jdpipe <jdpipe@2a3edb79-3362-4b52-9e0b-1d530fbd1d84>
2007-10-15 05:59:46 +0000

    Woops failed to correct #include statements

--- trunk/freesteam/example/example.cpp
+++ trunk/freesteam/example/example.cpp
@@ -19,9 +19,9 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 */
 
-#include "steamcalculator.h"
-#include "solver.h"
-#include "solver2.h"
+#include "freesteam/steamcalculator.h"
+#include "freesteam/solver.h"
+#include "freesteam/solver2.h"
 #include <iostream>
 #include <iomanip>
 using namespace std;
