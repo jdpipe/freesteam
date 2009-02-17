0d804f14b42356b40147f001f091d5646ea8e456
jdpipe <jdpipe@2a3edb79-3362-4b52-9e0b-1d530fbd1d84>
2009-02-17 00:45:56 +0000

    Fixed test.py for Linux.

--- freesteam/trunk/freesteam/python/test.py
+++ freesteam/trunk/freesteam/python/test.py
@@ -1,28 +1,23 @@
 #!/usr/bin/env python
 # trickery to add .. to LD_LIBRARY_PATH before attempting to load freesteam lib
 import os, sys, platform
-up = os.path.abspath(os.path.join(sys.path[0],'..'))
+
+if not os.path.exists(os.path.join(sys.path[0],'_freesteam.so')):
+	sys.stderr.write("\nLibrary _freesteam.so not present in %s\n" % sys.path[0])
+	sys.exit(1)
 
 if platform.system()=="Windows":
-	lib = '_freesteam.pyd'
-	pathvar = 'PATH'
-	exe = sys.executable
-else:
-	pathvar = 'LD_LIBARY_PATH'
-	lib = '_freesteam.so'
-	exe = 'python'
+	sys.stderr.write("\nBefore running tests on Windows, you need to your PATH\n")
+	sys.stderr.write("to include to location of freesteam.dll. In MSYS, you\n")
+	sys.stderr.write("can do this with 'export PATH=..:$PATH', assuming.\n")
+	sys.stderr.write("you are in the directory containing test.py.\n")
+	sys.exit(1)
 	
-if not os.environ.get(pathvar) or up not in os.environ.get(pathvar):
-	os.environ[pathvar] = up
-	script = os.path.join(sys.path[0],"test.py")
-	print "RESTARTING"
-	os.execvp(exe,[script] + sys.argv)
-else:
-	print "%s = %s" %(pathvar,os.environ.get(pathvar))
-
-if not os.path.exists(os.path.join(sys.path[0],lib)):
-	sys.stderr.write("Library _freesteam.so not present in %s\n" % sys.path[0])
-	sys.exit(1);
+up = os.path.abspath(os.path.join(sys.path[0],'..'))
+if not os.environ.get('LD_LIBRARY_PATH') or up not in os.environ.get('LD_LIBRARY_PATH'):
+	os.environ['LD_LIBRARY_PATH'] = up
+	script = os.path.join(sys.path[0],"test.py")					
+	os.execvp("python",[script] + sys.argv)
 
 #-------------------------------------------------------------------------------
 # now for the tests...
