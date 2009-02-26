65f83d0def7df97a2899a60a6200beb1dad42e78
jdpipe <jdpipe@2a3edb79-3362-4b52-9e0b-1d530fbd1d84>
2009-02-26 23:34:43 +0000

    Fixed drhof/dT and drhog/dT for all temperatures up to TCRIT. Created p-h diagram including saturation curves and 'quivers' for gradient direction (this seems to expose a bug in the quiver function).

--- branches/new-c/derivs.c
+++ branches/new-c/derivs.c
@@ -406,7 +406,7 @@ double freesteam_drhofdT_T(double T){
 		SteamState Sf = freesteam_region3_set_rhoT(rhof,T);
 		double dpdT = TV3('p',Sf);
 		double dpdrho = -1./SQ(rhof) * VT3('p',Sf);
-		return (dpsatdT + dpdT)/dpdrho;
+		return (dpsatdT - dpdT)/dpdrho;
 	}
 }
 
@@ -422,7 +422,7 @@ double freesteam_drhogdT_T(double T){
 		SteamState Sg = freesteam_region3_set_rhoT(rhog,T);
 		double dpdT = TV3('p',Sg);
 		double dpdrho = -1./SQ(rhog) * VT3('p',Sg);
-		return (dpsatdT + dpdT)/dpdrho;
+		return (dpsatdT - dpdT)/dpdrho;
 	}
 }
 
