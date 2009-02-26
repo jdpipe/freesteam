ffec74174cda85bfb31028fcf0247117f6711d9f
John Pye <john@curioussymbols.com>
2009-02-26 23:34:43 +0000

    Fixed drhof/dT and drhog/dT for all temperatures up to TCRIT. Created p-h diagram including saturation curves and 'quivers' for gradient direction (this seems to expose a bug in the quiver function).

--- python/phdiagram.py
+++ python/phdiagram.py
@@ -1,6 +1,45 @@
-from freesteam import *
-import pylab
+import freesteam
+import matplotlib
+matplotlib.use('gtkcairo')
+from pylab import *
+import math
 
-def hmin_p(p):
-	return region1_pT(p,TMIN).h
+figure()
+hold(1)
+n = 400
+TT = [273.15 + (freesteam.TCRIT - 273.15)*x/n for x in range(n+1)]
 
+hf = [freesteam.region4_Tx(T,0).h/1e3 for T in TT]
+hg = [freesteam.region4_Tx(T,1).h/1e3 for T in TT]
+plot(hf,TT,'b-')
+plot(hg,TT,'r-')
+
+pp = logspace(-3,3)*1e5
+hh = arange(50.,4000.,100)*1e3
+
+hh1 = arange(50.,4000.,20)*1e3
+
+x = []
+y = []
+u = []
+v = []
+for p in pp:
+	plot(hh1/1e3,[freesteam.steam_ph(p,h).T for h in hh1],alpha=0.2)
+	for h in hh:
+		try:
+			S = freesteam.steam_ph(p,h)
+			x += [S.h/1.e3]
+			y += [S.T]
+			dy = freesteam.freesteam_deriv(S,'T','h','p')
+			dx = 0.0005
+			m = math.sqrt(dx**2 + dy**2)
+			u += [dx/m]
+			v += [dy/m]
+		except:
+			pass
+
+quiver(x,y,u,v,alpha=0.6)
+axis([0,4000,273.15,800])
+xlabel('h / [kJ/kg]')
+ylabel('T / [K]')
+show()
--- python/satcurve.py
+++ python/satcurve.py
@@ -1,4 +1,5 @@
 import matplotlib
+# vim: encoding=utf-8
 matplotlib.use('gtkcairo')
 from pylab import *
 import freesteam
@@ -10,7 +11,12 @@ dpdT = [freesteam.dpsatdT_T(t)/1e6 for t in T]
 rhof = [freesteam.rhof_T(t) for t in T]
 rhog = [freesteam.rhog_T(t) for t in T]
 
+figure()
 plot(T,dpdT,'g-')
+xlabel("T")
+ylabel(unicode("∂p/∂T"))
+
+figure()
 hold(1)
 plot(T,rhof,'b-')
 plot(T,rhog,'r-')
@@ -24,5 +30,4 @@ plot(T,rhog,'r-')
 axis([273.15,freesteam.TCRIT,-20,+20])
 
 show()
-
 
