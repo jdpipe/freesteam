0a579c257cde10b8bcdaebbafb4ddce59ea2fb03
jdpipe <jdpipe@2a3edb79-3362-4b52-9e0b-1d530fbd1d84>
2009-03-09 04:16:22 +0000

    Added (p,s) and (p,T) diagrams (still need work).

---
+++ branches/new-c/python/pTdiagram.py
@@ -0,0 +1,40 @@
+import freesteam
+import matplotlib
+matplotlib.use('gtkcairo')
+from pylab import *
+import math
+
+figure()
+hold(1)
+n = 400
+TT0 = [273.15 + (freesteam.TCRIT - 273.15)*x/n for x in range(n+1)]
+psat = [freesteam.psat_T(T)/1e6 for T in TT0]
+
+Tmin = 273.15
+Tmax = 1073.15
+DT = Tmax - Tmin
+pmin = 1e-3*1e5
+pmax = 1e3*1e5
+pp = logspace(-3,3)*1.e5
+TT = arange(Tmin,Tmax,DT/100)
+im = zeros((len(TT),len(pp)))
+y = 0
+for p in pp:
+	print "p = %f MPa" % (p/1e6)
+	x = 0
+	for T in TT:
+		S = freesteam.steam_pT(p,T)
+		#print "p = %f, T = %f" % (p,T)
+		r = ord(S.region)
+		#print "p = %f MPa, T = %f K, region[%d,%d] = %d" % (p/1e6,T,x,y,r)
+		im[x,y] = float(r) / 4.
+		x += 1
+	y += 1
+
+imshow(im,extent=[Tmin,Tmax,pmin/1e6,pmax/1e6],cmap=autumn(),origin='lower',aspect='auto',interpolation='nearest')
+plot(TT0,psat,'b-')
+axis([Tmin,Tmax,pmin/1e6,pmax/1e6])
+#quiver(x,y,u,v,alpha=0.6)
+xlabel('T / [K]')
+ylabel('p / [MPa]')
+show()
