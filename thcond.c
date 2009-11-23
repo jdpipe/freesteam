b4ae808195956b1fef16414ce0fb59705d18afff
John Pye <john@curioussymbols.com>
2009-11-23 05:22:59 +0000

    Trying to track down the thcond error...

--- thcond.c
+++ thcond.c
@@ -36,8 +36,35 @@
 #define THCOND_RHOSTAR 317.7
 #define THCOND_KSTAR 1.0
 
+#define THCOND_a_COUNT 4
+const double THCOND_a[THCOND_a_COUNT] = {
+	0.0102811
+	,0.0299621
+	,0.0156146
+	,-0.00422464
+};
 
-double freesteam_k_rhoT(double rho, double T){
+#define THCOND_b0 -0.397070
+#define THCOND_b1 0.400302
+#define THCOND_b2 1.060000
+#define THCOND_B1 -0.171587
+#define THCOND_B2 2.392190
+
+#define THCOND_C1 0.642857
+#define THCOND_C2 -4.11717
+#define THCOND_C3 -6.17937
+#define THCOND_C4 0.00308976
+#define THCOND_C5 0.0822994
+#define THCOND_C6 10.0932
+
+#define THCOND_d1 0.0701309
+#define THCOND_d2 0.0118520
+#define THCOND_d3 0.00169937
+#define THCOND_d4 -1.0200
+
+
+/* freesteam code */
+double freesteam_xxxk_rhoT(double rho, double T){
 	double Tbar = T / THCOND_TSTAR;
 	double rhobar = rho / THCOND_RHOSTAR;
 
@@ -46,13 +73,6 @@ double freesteam_k_rhoT(double rho, double T){
 	double Tpow = Troot;
 	double lam0 = 0;
 
-#define THCOND_a_COUNT 4
-	const double THCOND_a[THCOND_a_COUNT] = {
-		0.0102811
-		,0.0299621
-		,0.0156146
-		,-0.00422464
-	};
 
 	int k;
 	for(k = 0; k < THCOND_a_COUNT; ++k) {
@@ -60,23 +80,11 @@ double freesteam_k_rhoT(double rho, double T){
 		Tpow *= Tbar;
 	}
 
-#define THCOND_b0 -0.397070
-#define THCOND_b1 0.400302
-#define THCOND_b2 1.060000
-#define THCOND_B1 -0.171587
-#define THCOND_B2 2.392190
 
 	double lam1 = THCOND_b0 + THCOND_b1 * rhobar + THCOND_b2 * exp(THCOND_B1 * SQ(rhobar + THCOND_B2));
-
-#define THCOND_C1 0.642857
-#define THCOND_C2 -4.11717
-#define THCOND_C3 -6.17937
-#define THCOND_C4 0.00308976
-#define THCOND_C5 0.0822994
-#define THCOND_C6 10.0932
 	
 	double DTbar = fabs(Tbar - 1) + THCOND_C4;
-	double DTbarpow = pow(DTbar, 0.6);
+	double DTbarpow = pow(DTbar, 3./5);
 	double Q = 2. + THCOND_C5 / DTbarpow;
 
 	double S;
@@ -86,28 +94,25 @@ double freesteam_k_rhoT(double rho, double T){
 		S = THCOND_C6 / DTbarpow;
 	}
 
-#define THCOND_d1 0.0701309
-#define THCOND_d2 0.0118520
-#define THCOND_d3 0.00169937
-#define THCOND_d4 -1.0200
-
+#if 0
 	double lam2 = 
-		(THCOND_d1 / pow(Tbar,10) + THCOND_d2) * pow(rhobar,1.8) * 
+		(THCOND_d1 / ipow(Tbar,10) + THCOND_d2) * pow(rhobar,1.8) * 
 			exp(THCOND_C1 * (1 - pow(rhobar,2.8)))
 		+ THCOND_d3 * S * pow(rhobar,Q) *
 			exp((Q/(1+Q))*(1 - pow(rhobar,1+Q)))
 		+ THCOND_d4 *
-			exp(THCOND_C2 * pow(Tbar,3./2) + THCOND_C3 / pow(rhobar,5));
+			exp(THCOND_C2 * ipow(Troot,3) + THCOND_C3 / ipow(rhobar,5));
+#endif
 
-#if 0
-	double lam2_1_1 = (THCOND_d1 / pow(Tbar,10) + THCOND_d2);
+#if 1
+	double lam2_1_1 = THCOND_d1 * pow(Tbar,-10) + THCOND_d2;
 
 #ifdef THCOND_DEBUG
-	fprintf(stderr,"pow(Tbar,10) = %f, lam2_1_1 = %f\n",pow(Tbar,10), lam2_1_1);
+	fprintf(stderr,"Q = %f, S = %f, pow(Tbar,10) = %f, lam2_1_1 = %f\n", Q, S, ipow(Tbar,10), lam2_1_1);
 #endif	
 
-	double lam2_1 = lam2_1_1 * pow(rhobar,1.8) * 
-			exp(THCOND_C1 * (1. - pow(rhobar,2.8)));
+	double lam2_1 = lam2_1_1 * pow(rhobar,9./5) * 
+			exp(THCOND_C1 * (1. - pow(rhobar,14./5)));
 		
 	double lam2_2 = THCOND_d3 * S * pow(rhobar,Q) *
 			exp((Q/(1.+Q))*(1. - pow(rhobar,1+Q)));
