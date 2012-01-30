dbfb8b99a8a809a820dd2cdfb8a6926211bad7a8
jdpipe <jdpipe@2a3edb79-3362-4b52-9e0b-1d530fbd1d84>
2012-01-30 21:53:45 +0000

    Updated GSL missing package warning, add missing #include in surftens.c.

--- trunk/surftens.c
+++ trunk/surftens.c
@@ -18,6 +18,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 */
 #define FREESTEAM_BUILDING_LIB
 #include "surftens.h"
+#include <math.h>
 
 /**
 	Calculate surface tension between water phase and vapour phase
