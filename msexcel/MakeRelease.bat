fd92461483a778a234e81f7ea282f1c64042854e
b2besses <b2besses@2a3edb79-3362-4b52-9e0b-1d530fbd1d84>
2013-05-16 18:19:02 +0000

    Create Makefiles for 32 and 64 bits xll builds, msexcel folder clean-up

--- msexcel/MakeRelease.bat
+++ msexcel/MakeRelease.bat
@@ -1,2 +1,7 @@
-mingw32-make > MakeRelease.log
+del MakeRelease.log
+mingw32-make -f Makefile-win32.mak clean >> MakeRelease.log
+mingw32-make -f Makefile-win32.mak >> MakeRelease.log
+
+mingw32-make -f Makefile-win64.mak clean >> MakeRelease.log
+mingw32-make -f Makefile-win64.mak >> MakeRelease.log
 pause
