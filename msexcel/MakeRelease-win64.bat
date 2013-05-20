ff02ae2d4d04e59aa859164afb9a278780cc71ed
b2besses <b2besses@2a3edb79-3362-4b52-9e0b-1d530fbd1d84>
2013-05-20 08:29:59 +0000

    Use different obj folder for 32 and 64 bits .o files

--- msexcel/MakeRelease-win64.bat
+++ msexcel/MakeRelease-win64.bat
@@ -1,4 +1,2 @@
-del MakeRelease-win64.log
-mingw32-make PLATFORM=WIN64 clean >> MakeRelease-win64.log
-mingw32-make PLATFORM=WIN64 >> MakeRelease-win64.log
-pause
+mingw32-make PLATFORM=WIN64
+@pause
